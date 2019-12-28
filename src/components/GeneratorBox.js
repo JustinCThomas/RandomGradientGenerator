540a9ad3a97f3a48d6c25894ccbf734ef511fb8e
Justin Thomas <thomascjustin@gmail.com>
2019-12-27 22:31:33 -0500

    Add Demo Background Color Generation

--- src/components/GeneratorBox.js
+++ src/components/GeneratorBox.js
@@ -1,9 +1,14 @@
 import React from "react"
 
 function GeneratorBox() {
+
+  function generateGradient() {
+    document.body.style.backgroundColor = "red";
+  }
+
   return (
     <div className="generator-box">
-      <button id="generator-button">Generate Gradient</button>
+      <button id="generator-button" onClick={generateGradient}>Generate Gradient</button>
     </div>
   )
 }
